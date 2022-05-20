024b023b08c4a792667c36e0c6011c3ea5b516fe
bglynn <bglynn@gmail.com>
2022-05-20 16:51:03 -0700

    tf parse test

---
+++ resources/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.4"
+}
+
+provider "aws" {
+  region              = "us-east-1"
+  allowed_account_ids = ["111111111111"]
+}
